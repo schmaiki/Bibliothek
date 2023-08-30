9156d40cc574dcfeb2dc1a8aa7403c80dd83f0f1
Maiki <maikpeters1511@gmail.com>
2023-08-30 10:28:41 +0200

    feat(buch): add Buch model with validation

--- Bibliothek.API/Models/Buch/Buch.cs
+++ Bibliothek.API/Models/Buch/Buch.cs
@@ -1,13 +1,22 @@
-﻿namespace Bibliothek.API.Models.Buch;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bibliothek.API.Models.Buch;
 
 public class Buch
 {
+    [Key]
     public int Id { get; set; }
 
+    [MaxLength(100)]
+    [Required]
     public string Titel { get; set; }
-    
+
+    [MaxLength(100)]
+    [Required]
     public string ISBN { get; set; }
 
+    [Required]
+    [DataType(DataType.Date)]
     public DateOnly Erscheinungsdatum { get; set; }
 
     public Herausgeber Herausgeber { get; set; }
